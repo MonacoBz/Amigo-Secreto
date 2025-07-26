e6a76e5190fec6d9bde2a1b25f0f900b4ceff66b
Roque Betanzos <gerardossj2@hotmail.com>
2025-07-25 22:33:16 -0600

    Se agrego funcionalidad a agregarAmigo

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+function agregarAmigo(){
+    let input = document.getElementById("amigo");
+    let amigo = input.value
+    if(amigo.trim() !== ""){
+        amigos.push(amigo);
+        input.value=""
+        return;
+    }
+    alert("cadena vacia, porfavor agrega un nombre")
+    return;
+}
+
